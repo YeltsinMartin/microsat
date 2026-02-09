b403b351ebd53e9acf4514305c040de7471d32ef
Yeltsin Martin babu <yelasterix@gmail.com>
2026-02-09 19:21:56 +0000

    delete local build files

--- build/obc/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(PROJECT_PATH)/obc/inc
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/obc -lobc
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += obc
-COMPONENT_LDFRAGMENTS += 
-component-obc-build: 
--- build/obc/src/imu.d
+++
@@ -1,5 +0,0 @@
-src/imu.o: /Users/yeltsin/Documents/Code/esp/microsat/obc/src/imu.cpp \
- /Users/yeltsin/Documents/Code/esp/microsat/obc/inc/imu.h
-
-/Users/yeltsin/Documents/Code/esp/microsat/obc/inc/imu.h:
-/Users/yeltsin/Documents/Code/esp/microsat/obc/src/imu.cpp:
